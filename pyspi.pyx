e76e9312e93de39d13a4cc1411b4c1096dddef6c
davidjun <davejun@gmail.com>
2019-06-27 10:57:57 -0500

    cython wrapper fully working

--- pyspi.pyx
+++ pyspi.pyx
@@ -1,9 +1,11 @@
+from libc.stdint cimport uint32_t, uint8_t
+
 cdef extern from "spi.h":
     int spi_setup(const char *device, uint32_t speed)
     int spi_write(int fd, uint8_t *tx, int len)
 
-def py_spi_setup(device: bytes, speed: int) -> int:
+def pyspi_setup(device: bytes, speed: int) -> int:
     return spi_setup(device, speed)
 
-def py_spi_write(fd: int, tx: bytes, len: int) -> int:
-    return spi_setup(fd, tx, len)
+def pyspi_write(fd: int, tx: bytes, len: int) -> int:
+    return spi_write(fd, tx, len)
